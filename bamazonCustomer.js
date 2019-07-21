3b1e21093de7ef87b3b6aae380a42cc5b16c4a21
Michael Morris <mmorris1990@gmail.com>
2019-07-21 09:32:18 -0500

    reworked function to update the db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,49 +1,143 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var fs = require("fs");
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
+var Table = require("cli-table");
 
-// Link to password file so password is not here
-async function getPassword() {
-    return await readFile('pass.txt', 'utf8');
-    // 
-    // 
+// Create connection to db
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "7283",
+    database: "bamazon"
+});
+
+// Test connection
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(" =================== \n Welcome to Bamazon! \n =================== \n");
+    welcome();
+});
+
+function welcome() {
+
+    // Ask customer what they'd like to do
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            choices: ["View items for sale", "Leave the store"],
+            message: "Please select what you would like to do.\n"
+        }
+    ]).then(function (action) {
+
+        // If user wants to view items, run rest of app
+        if (action.action === "View items for sale") {
+            viewItems();
+
+            // If user wants to leave, exit app
+        } else if (action.action === "Leave the store") {
+            exit();
+        };
+    });
 };
 
-getPassword().then(data => {
-    console.log(data);
-    var pass = data;
-    
-    // create connection to database
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: pass,
-        database: "bamazon"
-    });
-    
-    // Test connection (DELETE LATER)
-    connection.connect(function (err) {
+var viewItems = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log("connected as id " + connection.threadId);
-        start();
-    });
-    
-    // Main app functionality
-    function start() {
-        var query = "SELECT item_id, product_name, price FROM products";
-        connection.query(query, function (err, res) {
-            if (err) throw err;
-            console.log("********************" + "\nCurrent Stock For Sale" + "\n--------------------");
-            for (var i=0; i < res.length; i++) {
-                console.log("Item ID: " + res[i].item_id + "    Item Name: " + res[i].product_name + "    Price: $" + res[i].price);
-            };
-            console.log("********************")
-    
-        })
-        connection.end();
-    }
 
-});
+        // Create table to display current stock
+        var table = new Table({
+            head: ["ID", "Product Name", "Department", "Price", "In Stock"]
+        });
+        console.log("\nHere is what we have for sale! ");
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price.toFixed(2), res[i].stock_quantity]);
+        }
+        console.log(table.toString());
+
+        // Ask customer what they'd like to buy and how much
+        inquirer.prompt([
+            {
+                name: "id",
+                message: "\nPlease enter the ID of the item that you would like to purchase.",
+
+                // Validates that the input is an available ID number
+                validate: function (value) {
+                    if (value > 0 && isNaN(value) === false && value <= res.length) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "qty",
+                message: "\nHow many would you like?",
+
+                validate: function (value) {
+                    if (value > 0 && isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+
+        ]).then(function (transaction) {
+
+            var itemQty;
+            var itemName;
+            var itemPrice;
+
+            // Set above vars equal to results where the input id matches db id
+            for (var j = 0; j < res.length; j++) {
+                if (parseInt(transaction.id) === res[j].item_id) {
+                    itemName = res[j].product_name;
+                    itemPrice = res[j].price;
+                    itemQty = res[j].stock_quantity;
+                }
+            }
+
+            // If user tries to buy more qty than db has available, tell them no, run the welcome function again
+            if (parseInt(transaction.qty) > itemQty) {
+                console.log("\nInsufficient inventory for your requested quantity. We have "
+                    + itemQty + " in stock. Try again.\n");
+                welcome();
+            }
+
+            // If user tries to buy equal/less qty than db has available, tell them yes
+            else if (parseInt(transaction.qty) <= itemQty) {
+                console.log("\nSuccess! You purchased " + transaction.qty
+                    + " of " + itemName + " for $" + itemPrice + "!\n");
+
+                lowerQty(transaction.id, transaction.qty, itemQty, itemPrice);
+
+                welcome();
+            }
+        });
+    });
+};
+
+function lowerQty(item, purchaseQty, stockQty) {
+
+    // Connect to the db and reduce stock of purchased item by amount @ ID #
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: stockQty - parseInt(purchaseQty)
+            },
+            {
+                item_id: parseInt(item)
+            }
+        ],
+
+        function (err, res) {
+            if (err) throw error;
+        });
+}
+
+// Exit function that will close the connection to the db
+function exit() {
+    console.log("\nThanks for stopping by! Have a good day.");
+    connection.end();
+};
